e566f33df0c4fd808cf4d9e816633d2e4283f68c
edzempel <edward.zempel@gmail.com>
2019-11-14 20:44:24 -0600

    Roster.java implement all methods

--- src/edu/metrostate/ics425/p4/ejz360/model/Roster.java
+++ src/edu/metrostate/ics425/p4/ejz360/model/Roster.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -15,13 +16,19 @@ public class Roster {
 	private String url = "jdbc:mysql://localhost/foam";
 	private String user = "root";
 	private String password = "";
-	private String query = "SELECT NationalID, FirstName, LastName, DateOfBirth FROM foam.Athletes";
+	private final String SELECT_ALL_QUERY = "SELECT NationalID, FirstName, LastName, DateOfBirth FROM foam.Athletes";
 
+//	 1. Connect to the database
+//	 2. create query statement
+//	 3. execute query
+//	 4. display the results
+//	 5. close the results and query and the connection
+	
 	public List<Athlete> findAll() {
 		List<Athlete> athletes;
 		try (Connection con = DriverManager.getConnection(url, user, password);
 				Statement stmt = con.createStatement();
-				ResultSet rs = stmt.executeQuery(query);) {
+				ResultSet rs = stmt.executeQuery(SELECT_ALL_QUERY);) {
 			athletes = getAthletes(rs);
 		} catch (SQLException e) {
 			athletes = null;
@@ -33,7 +40,7 @@ public class Roster {
 		boolean output = false;
 		try (Connection con = DriverManager.getConnection(url, user, password);
 				Statement stmt = con.createStatement();
-				ResultSet rs = stmt.executeQuery(query);) {
+				ResultSet rs = stmt.executeQuery(SELECT_ALL_QUERY);) {
 			while (rs.next()) {
 				if (rs.getString("NationalID").equalsIgnoreCase(id)) {
 					output = true;
@@ -48,69 +55,105 @@ public class Roster {
 	}
 
 	public Athlete find(String id) {
-		// TODO: implement find
-		return null;
+		AthleteBean athlete = new AthleteBean();
+
+		String findQuery = "Select NationalID, FirstName, LastName, DateOfBirth "
+				+ "FROM foam.Athletes WHERE NationalID = ?";
+		if (id != null && isOnRoster(id)) {
+			try (Connection con = DriverManager.getConnection(url, user, password);
+					PreparedStatement ps = getFindPreparedStatement(con, findQuery, id);
+					ResultSet rs = ps.executeQuery();) {
+				while (rs.next()) {
+					String NationalID = rs.getString("NationalID");
+					String FirstName = rs.getString("FirstName");
+					String LastName = rs.getString("LastName");
+					LocalDate dob = convertToLocalDateViaSqlDate(rs.getDate("DateOfBirth"));
+					athlete.setNationalID(NationalID);
+					athlete.setFirstName(FirstName);
+					athlete.setLastName(LastName);
+					athlete.setDateOfBirth(dob);
+
+				}
+
+			} catch (SQLException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+
+		return athlete;
+	}
+
+	private PreparedStatement getFindPreparedStatement(Connection con, String query, String id) throws SQLException {
+
+		PreparedStatement ps = con.prepareStatement(query);
+		ps.setString(1, id);
+
+		return ps;
 	}
 
 	public boolean delete(String id) {
-		// TODO: implement delete
-		return false;
+		int deleted = 0;
+		String deleteQuery = "DELETE FROM foam.Athletes WHERE NationalID = ?";
+		if (id != null && isOnRoster(id)) {
+			try (Connection con = DriverManager.getConnection(url, user, password);
+					PreparedStatement ps = getFindPreparedStatement(con, deleteQuery, id);) {
+				deleted = ps.executeUpdate();
+
+			} catch (SQLException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+		return deleted == 1 ? true : false;
 	}
 
 	public boolean update(Athlete anAthlete) {
-		// TODO: implement update
-		return false;
+		int updated = 0;
+		String updateQuery = "UPDATE foam.Athletes SET " + "FirstName = ?, " + "LastName = ?, " + "DateOfBirth = ? "
+				+ "WHERE NationalID = ?";
+		if (anAthlete != null && isOnRoster(anAthlete.getNationalID())) {
+			try (Connection con = DriverManager.getConnection(url, user, password);
+					Statement stmt = con.createStatement();
+					PreparedStatement ps = con.prepareStatement(updateQuery);) {
+				ps.setString(1, anAthlete.getFirstName());
+				ps.setString(2, anAthlete.getLastName());
+				ps.setDate(3, convertToDateViaSqlDate(anAthlete.getDateOfBirth()));
+				ps.setString(4, anAthlete.getNationalID());
+
+				updated = ps.executeUpdate();
+
+			} catch (SQLException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+
+		return updated == 1 ? true : false;
 
 	}
 
 	public boolean add(Athlete anAthlete) {
-		// TODO: implement add
-		return false;
-	}
+		int added = 0;
+		String addQuery = "INSERT INTO foam.Athletes (NationalID, FirstName, LastName, DateOfBirth) "
+				+ "VALUES (?,?,?,?)";
+		if (anAthlete != null && !isOnRoster(anAthlete.getNationalID())) {
+			try (Connection con = DriverManager.getConnection(url, user, password);
+					Statement stmt = con.createStatement();
+					PreparedStatement ps = con.prepareStatement(addQuery);) {
+				ps.setString(1, anAthlete.getNationalID());
+				ps.setString(2, anAthlete.getFirstName());
+				ps.setString(3, anAthlete.getLastName());
+				ps.setDate(4, convertToDateViaSqlDate(anAthlete.getDateOfBirth()));
 
-	public static void main(String[] args) {
-		// 1. Connect to the database
-		// 2. create query statement
-		// 3. execute query
-		// 4. display the results
-		// 5. close the results and query and the connection
+				added = ps.executeUpdate();
 
-//		String url = "jdbc:mysql://localhost/foam";
-//		String user = "root";
-//		String password = "";
-//		String query = "SELECT NationalID, FirstName, LastName, DateOfBirth FROM foam.Athletes";
-//
-//		try (Connection con = DriverManager.getConnection(url, user, password);
-//				Statement stmt = con.createStatement();
-//				ResultSet rs = stmt.executeQuery(query);) { // user for Read, other CUD actions use executeUpdate()
-//															// AutoClosable resources can be declared in try with
-//															// resources
-//
-//			// turn the results into Athletes, results are kind of like an iterator
-//			// results start before the first record, returns true if there is a 'next'
-//			// record
-//			while (rs.next()) { // true while records are available to be processed
-//				String nationalID = rs.getString("NationalID");
-//				String firstName = rs.getString("FirstName");
-//				String lastName = rs.getString("LastName");
-//				LocalDate dob = LocalDate.parse(rs.getDate("DateOfBirth").toString()); // figure out how to convert a
-//				// Date/SQL date to LocalDate
-//				System.out.printf("%s\t%s\t%s\t%s\n", nationalID, firstName, lastName, dob);
-//			}
-//
-//		} catch (
-//
-//		SQLException e) {
-//
-//			e.printStackTrace();
-//		}
-		Roster rd = new Roster();
-		List<Athlete> athletes = rd.findAll();
-		for (Athlete athlete : athletes) {
-			System.out.println((AthleteBean) athlete);
+			} catch (SQLException e) {
+				System.out.println(e.getMessage());
+			}
 		}
 
+		return added == 1 ? true : false;
 	}
+
+	
 
 	private List<Athlete> getAthletes(ResultSet rs) throws SQLException {
 		ArrayList<Athlete> athletes = new ArrayList<Athlete>();
@@ -120,7 +163,8 @@ public class Roster {
 			String nationalID = rs.getString("NationalID");
 			String firstName = rs.getString("FirstName");
 			String lastName = rs.getString("LastName");
-			LocalDate dob = LocalDate.parse(rs.getDate("DateOfBirth").toString());
+//			LocalDate dob = LocalDate.parse(rs.getDate("DateOfBirth").toString());
+			LocalDate dob = convertToLocalDateViaSqlDate(rs.getDate("DateOfBirth"));
 			athlete.setNationalID(nationalID);
 			athlete.setLastName(lastName);
 			athlete.setFirstName(firstName);
@@ -130,5 +174,13 @@ public class Roster {
 
 		return athletes;
 
+	}
+
+	private LocalDate convertToLocalDateViaSqlDate(java.sql.Date dateToConvert) {
+		return new java.sql.Date(dateToConvert.getTime()).toLocalDate();
+	}
+
+	private java.sql.Date convertToDateViaSqlDate(LocalDate dateToConvert) {
+		return java.sql.Date.valueOf(dateToConvert);
 	}
 }
