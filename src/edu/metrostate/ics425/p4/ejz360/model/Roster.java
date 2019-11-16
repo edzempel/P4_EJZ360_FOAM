8f447420664f4571cb02fa4a8e667a8f699e034a
edzempel <edward.zempel@gmail.com>
2019-11-16 12:26:24 -0600

    Roster.java Use Connection Pool

--- src/edu/metrostate/ics425/p4/ejz360/model/Roster.java
+++ src/edu/metrostate/ics425/p4/ejz360/model/Roster.java
@@ -2,31 +2,53 @@ package edu.metrostate.ics425.p4.ejz360.model;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
+import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDate;
 
+
 import edu.metrostate.ics425.foam.model.Athlete;
 
 public class Roster {
-	private String url = "jdbc:mysql://localhost/foam";
-	private String user = "root";
-	private String password = "";
+//	private String url = "jdbc:mysql://localhost/foam";
+//	private String user = "root";
+//	private String password = "";
 	private final String SELECT_ALL_QUERY = "SELECT NationalID, FirstName, LastName, DateOfBirth FROM foam.Athletes";
+	
+	private DataSource dataSource = getDataSource();
+
+	private DataSource getDataSource() {
+		DataSource dataSource = null;
+		try {
+			Context initContext = new InitialContext();
+			Context envContext = (Context) initContext.lookup("java:/comp/env");
+			dataSource = (DataSource) envContext.lookup("jdbc/FoamDB");
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return dataSource;
+	}
 
 //	 1. Connect to the database
 //	 2. create query statement
 //	 3. execute query
 //	 4. display the results
 //	 5. close the results and query and the connection
-	
+
 	public List<Athlete> findAll() {
 		List<Athlete> athletes;
-		try (Connection con = DriverManager.getConnection(url, user, password);
+		try (Connection con = dataSource.getConnection();
+//				Connection con = DriverManager.getConnection(url, user, password);
 				Statement stmt = con.createStatement();
 				ResultSet rs = stmt.executeQuery(SELECT_ALL_QUERY);) {
 			athletes = getAthletes(rs);
@@ -38,7 +60,8 @@ public class Roster {
 
 	public boolean isOnRoster(String id) {
 		boolean output = false;
-		try (Connection con = DriverManager.getConnection(url, user, password);
+		try (Connection con = dataSource.getConnection();
+//				Connection con = DriverManager.getConnection(url, user, password);
 				Statement stmt = con.createStatement();
 				ResultSet rs = stmt.executeQuery(SELECT_ALL_QUERY);) {
 			while (rs.next()) {
@@ -60,7 +83,8 @@ public class Roster {
 		String findQuery = "Select NationalID, FirstName, LastName, DateOfBirth "
 				+ "FROM foam.Athletes WHERE NationalID = ?";
 		if (id != null && isOnRoster(id)) {
-			try (Connection con = DriverManager.getConnection(url, user, password);
+			try (Connection con = dataSource.getConnection();
+//					Connection con = DriverManager.getConnection(url, user, password);
 					PreparedStatement ps = getFindPreparedStatement(con, findQuery, id);
 					ResultSet rs = ps.executeQuery();) {
 				while (rs.next()) {
@@ -95,7 +119,8 @@ public class Roster {
 		int deleted = 0;
 		String deleteQuery = "DELETE FROM foam.Athletes WHERE NationalID = ?";
 		if (id != null && isOnRoster(id)) {
-			try (Connection con = DriverManager.getConnection(url, user, password);
+			try (Connection con = dataSource.getConnection();
+//					Connection con = DriverManager.getConnection(url, user, password);
 					PreparedStatement ps = getFindPreparedStatement(con, deleteQuery, id);) {
 				deleted = ps.executeUpdate();
 
@@ -111,7 +136,8 @@ public class Roster {
 		String updateQuery = "UPDATE foam.Athletes SET " + "FirstName = ?, " + "LastName = ?, " + "DateOfBirth = ? "
 				+ "WHERE NationalID = ?";
 		if (anAthlete != null && isOnRoster(anAthlete.getNationalID())) {
-			try (Connection con = DriverManager.getConnection(url, user, password);
+			try (Connection con = dataSource.getConnection();
+//					Connection con = DriverManager.getConnection(url, user, password);
 					Statement stmt = con.createStatement();
 					PreparedStatement ps = con.prepareStatement(updateQuery);) {
 				ps.setString(1, anAthlete.getFirstName());
@@ -135,7 +161,8 @@ public class Roster {
 		String addQuery = "INSERT INTO foam.Athletes (NationalID, FirstName, LastName, DateOfBirth) "
 				+ "VALUES (?,?,?,?)";
 		if (anAthlete != null && !isOnRoster(anAthlete.getNationalID())) {
-			try (Connection con = DriverManager.getConnection(url, user, password);
+			try (Connection con = dataSource.getConnection();
+//					Connection con = DriverManager.getConnection(url, user, password);
 					Statement stmt = con.createStatement();
 					PreparedStatement ps = con.prepareStatement(addQuery);) {
 				ps.setString(1, anAthlete.getNationalID());
@@ -153,8 +180,6 @@ public class Roster {
 		return added == 1 ? true : false;
 	}
 
-	
-
 	private List<Athlete> getAthletes(ResultSet rs) throws SQLException {
 		ArrayList<Athlete> athletes = new ArrayList<Athlete>();
 
@@ -164,7 +189,8 @@ public class Roster {
 			String firstName = rs.getString("FirstName");
 			String lastName = rs.getString("LastName");
 //			LocalDate dob = LocalDate.parse(rs.getDate("DateOfBirth").toString());
-			LocalDate dob = convertToLocalDateViaSqlDate(rs.getDate("DateOfBirth"));
+			Date date = rs.getDate("DateOfBirth");
+			LocalDate dob = convertToLocalDateViaSqlDate(date);
 			athlete.setNationalID(nationalID);
 			athlete.setLastName(lastName);
 			athlete.setFirstName(firstName);
@@ -176,11 +202,12 @@ public class Roster {
 
 	}
 
-	private LocalDate convertToLocalDateViaSqlDate(java.sql.Date dateToConvert) {
-		return new java.sql.Date(dateToConvert.getTime()).toLocalDate();
+	private LocalDate convertToLocalDateViaSqlDate(Date dateToConvert) {
+		LocalDate localDate = dateToConvert.toLocalDate();
+		return localDate;
 	}
 
-	private java.sql.Date convertToDateViaSqlDate(LocalDate dateToConvert) {
-		return java.sql.Date.valueOf(dateToConvert);
+	private Date convertToDateViaSqlDate(LocalDate dateToConvert) {
+		return Date.valueOf(dateToConvert);
 	}
 }
