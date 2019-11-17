484cb171429de6ef5a966cf839eb4429158e9880
edzempel <edward.zempel@gmail.com>
2019-11-17 16:09:30 -0600

    Roster.java add javadocs

--- src/edu/metrostate/ics425/p4/ejz360/model/Roster.java
+++ src/edu/metrostate/ics425/p4/ejz360/model/Roster.java
@@ -19,7 +19,8 @@ import java.time.LocalDate;
 import edu.metrostate.ics425.foam.model.Athlete;
 
 /**
- * P4 Roster Class provides connectivity to foam to MySQL database
+ * P4 Roster Class provides connectivity to foam application to MySQL database
+ * 
  * 
  * @author ezempel
  *
@@ -64,11 +65,6 @@ public class Roster {
 		this.testMode = mode;
 	}
 
-//	 1. Connect to the database
-//	 2. create query statement
-//	 3. execute query
-//	 4. display the results
-//	 5. close the results and query and the connection
 	/**
 	 * Returns a list containing all athletes on the roster
 	 * 
@@ -77,7 +73,6 @@ public class Roster {
 	public List<Athlete> findAll() {
 		List<Athlete> athletes;
 		try (Connection con = getConn();
-
 				Statement stmt = con.createStatement();
 				ResultSet rs = stmt.executeQuery(SELECT_ALL_QUERY);) {
 			athletes = getAthletes(rs);
@@ -87,6 +82,12 @@ public class Roster {
 		return athletes;
 	}
 
+	/**
+	 * Checks whether the specified athlete is on the roster
+	 * 
+	 * @param id NationalID of athlete
+	 * @return true if the athlete is on the roster, false if not
+	 */
 	public boolean isOnRoster(String id) {
 		boolean output = false;
 		try (Connection con = getConn();
@@ -105,12 +106,19 @@ public class Roster {
 		return output;
 	}
 
+	/**
+	 * Returns the athlete record for the athlete with the specified ID
+	 * 
+	 * @param id NationalID of the athlete
+	 * @return record for athlete, or null if not found
+	 */
 	public Athlete find(String id) {
-		AthleteBean athlete = new AthleteBean();
+		AthleteBean athlete = null;
 
 		String findQuery = "Select NationalID, FirstName, LastName, DateOfBirth "
 				+ "FROM foam.Athletes WHERE NationalID = ?";
 		if (id != null && isOnRoster(id)) {
+			athlete = new AthleteBean();
 			try (Connection con = getConn();
 
 					PreparedStatement ps = getFindPreparedStatement(con, findQuery, id);
@@ -135,14 +143,12 @@ public class Roster {
 		return athlete;
 	}
 
-	private PreparedStatement getFindPreparedStatement(Connection con, String query, String id) throws SQLException {
-
-		PreparedStatement ps = con.prepareStatement(query);
-		ps.setString(1, id);
-
-		return ps;
-	}
-
+	/**
+	 * Removes the record with the specified NationalID from the roster
+	 * 
+	 * @param id - NationalID of the athlete to be removed
+	 * @return true if the athlete was successfully removed
+	 */
 	public boolean delete(String id) {
 		int deleted = 0;
 		String deleteQuery = "DELETE FROM foam.Athletes WHERE NationalID = ?";
@@ -159,6 +165,13 @@ public class Roster {
 		return deleted == 1 ? true : false;
 	}
 
+	/**
+	 * Replaces existing record for the athlete with the record specified. The
+	 * NationalID of the athlete cannot be changed.
+	 * 
+	 * @param anAthlete - updated athlete record
+	 * @return true if the athlete was successfully updated
+	 */
 	public boolean update(Athlete anAthlete) {
 		int updated = 0;
 		String updateQuery = "UPDATE foam.Athletes SET " + "FirstName = ?, " + "LastName = ?, " + "DateOfBirth = ? "
@@ -202,7 +215,9 @@ public class Roster {
 				ps.setString(1, anAthlete.getNationalID());
 				ps.setString(2, anAthlete.getFirstName());
 				ps.setString(3, anAthlete.getLastName());
-				ps.setDate(4, convertToDateViaSqlDate(anAthlete.getDateOfBirth()));
+
+				LocalDate dateOfBirth = anAthlete.getDateOfBirth();
+				ps.setDate(4, convertToDateViaSqlDate(dateOfBirth));
 
 				added = ps.executeUpdate();
 
@@ -236,11 +251,18 @@ public class Roster {
 	}
 
 	private LocalDate convertToLocalDateViaSqlDate(Date dateToConvert) {
-		LocalDate localDate = dateToConvert.toLocalDate();
-		return localDate;
+		return dateToConvert != null ? dateToConvert.toLocalDate() : null;
 	}
 
 	private Date convertToDateViaSqlDate(LocalDate dateToConvert) {
-		return Date.valueOf(dateToConvert);
+		return dateToConvert != null ? Date.valueOf(dateToConvert) : null;
+	}
+
+	private PreparedStatement getFindPreparedStatement(Connection con, String query, String id) throws SQLException {
+
+		PreparedStatement ps = con.prepareStatement(query);
+		ps.setString(1, id);
+
+		return ps;
 	}
 }
